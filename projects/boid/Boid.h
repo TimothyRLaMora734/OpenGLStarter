95d62e48efc90056f7fe899e5ec673d0ea5c1c54
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2020-02-09 18:03:17 -0300

    fixed boid compilation on mingw

--- projects/boid/Boid.h
+++ projects/boid/Boid.h
@@ -9,36 +9,38 @@ public:
     vec3 pos;
     vec3 dir;
     float vel;
-    
+
     float visionDistanceSquared;
     float visionAngleCos;
-    
+
     Boid(vec3 pos,vec3 dir, float vel,
          float visionDistance,
          float visionAngleDegree) {
         this->pos = pos;
         this->dir = dir;
         this->vel = vel;
-        
+
         this->visionDistanceSquared = visionDistance*visionDistance;
         this->visionAngleCos = cos(DEG2RAD(visionAngleDegree / 2.0f));
     }
-    
+
     bool isVisible(const Boid &boid, bool ignoreAngle = false)const{
         vec3 toBoid = boid.pos - pos;
         float distance = sqrLength(toBoid);
         if (distance < visionDistanceSquared && distance > 1e-6f){
-            
+
             if (ignoreAngle)
                 return true;
-            
+
             distance = sqrt(distance);
             toBoid /= distance;
-            
+
             return dot(toBoid,dir) > visionAngleCos;
         }
         return false;
     }
+
+    SSE2_CLASS_NEW_OPERATOR
 };
 
 #endif
